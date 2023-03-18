fe05e5dee8f6d0caf72671c2cc5b976a60bbd3f4
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-03-18 18:17:09 +0000

    Add cookie autoclick loop

--- main.py
+++ main.py
@@ -10,3 +10,28 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+run = True
+while run:
+    cookies_total = driver.find_element(By.CSS_SELECTOR, "#money")
+    cookies = int(cookies_total.text)
+    cps = driver.find_element(By.CSS_SELECTOR, "#cps")
+    cps_text = cps.text.split(": ")
+    cookies_per_second = float(cps_text[1])
+
+    # Cookie autoclick
+    cookie = driver.find_element(By.CSS_SELECTOR, "#cookie")
+    cookie.click()
+
+
+
+
+
+
+
+
+
+
+
+
+driver.quit()
